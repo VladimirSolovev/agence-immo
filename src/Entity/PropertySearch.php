bc5cfb469b49db8ec53b7d092965cf7e46ab2597
Vladimir Solovev <vladimirsolovyev9@gmail.com>
2019-04-24 11:24:48 +0200

    Adding the option search filter.

--- src/Entity/PropertySearch.php
+++ src/Entity/PropertySearch.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class PropertySearch
@@ -16,6 +17,16 @@ class PropertySearch
      * @Assert\Range(min=10, max=400)
      */
     private $minSurface;
+
+    /**
+     * @var ArrayCollection
+     */
+    private $options;
+
+    public function __construct()
+    {
+        $this->options = new ArrayCollection();
+    }
 
     /**
      * @return int|null
@@ -53,5 +64,22 @@ class PropertySearch
         return $this;
     }
 
+    /**
+     * @return ArrayCollection
+     */
+    public function getOptions(): ArrayCollection
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param ArrayCollection $options
+     * @return PropertySearch
+     */
+    public function setOptions(ArrayCollection $options): PropertySearch
+    {
+        $this->options = $options;
+        return $this;
+    }
 
 }
