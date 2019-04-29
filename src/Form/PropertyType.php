26db03b117bf9a6a03af2acb945b560db7a45d50
Vladimir Solovev <vladimirsolovyev9@gmail.com>
2019-04-29 11:23:59 +0200

    Adding a hero image to the properties.

--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -28,6 +28,7 @@ class PropertyType extends AbstractType
             ])
             ->add('options', EntityType::class, [
                 'class' => Option::class,
+                'required' => false,
                 'choice_label' => 'name',
                 'multiple' => true
             ])
